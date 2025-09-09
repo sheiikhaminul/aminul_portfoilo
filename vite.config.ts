c7d113f82d09b8e87def64706e05cd89bb7f0424
Sheikh Aminul Islam <94759040+sheiikhaminul@users.noreply.github.com>
2025-09-10 00:43:03 +0600

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080, // dev server port
+    allowedHosts: ["9k7mc4-8080.csb.app"], // <- add your sandbox host here
+  },
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
